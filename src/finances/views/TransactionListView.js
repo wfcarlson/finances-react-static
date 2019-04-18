c95fc2a79d08445984a50200eb3f0a72f73f41eb
Walter <walterc@vt.edu>
2019-04-18 15:37:59 -0400

    new table

--- src/finances/views/TransactionListView.js
+++ src/finances/views/TransactionListView.js
@@ -33,21 +33,24 @@ class TransactionListView extends Component {
     }
 
     setEdit = (transaction) => {
-        return () => {
+        return (event) => {
+            event.stopPropagation();
             this.setState({editing: transaction});
         }
     }
 
     handleEdit = (attribute) => {
         return (event) => {
+            event.stopPropagation();
             var editing = this.state.editing;
             editing[attribute] = event.target.value;
             this.setState({editing: editing});
         }
     }
 
-    saveEdit = () => {
-                    
+    saveEdit = (event) => {
+        event.stopPropagation();
+     
         var data = {
             method: "PATCH",
             headers: {
@@ -96,135 +99,128 @@ class TransactionListView extends Component {
         }
     }
 
+    suppressClick = (event) => {
+        event.stopPropagation();
+    }
+
     render () {
         return (
             <div style={{ display: 'flex', flexDirection: 'column', alignContent: 'center' }} >
                 <h1>Incomes</h1>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Date</th>
-                            <th>Amount</th>
-                            <th>Name</th>
-                            <th>Category</th>
-                            <th>Delete</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.props.incomes.map((income) => 
-                            
-                            (<tr style={{ cursor:'pointer' }} onClick={this.setEdit(income)} key={income['transaction_id']}>
-                                <td>{income['date']}</td>
-                                <td>{income['amount']}</td>
-                                <td>
-                                    { this.state.editing.transaction_id === income.transaction_id ? 
-                                        (
-                                            <input type='text' value={this.state.editing.name} onChange={ this.handleEdit('name') }></input>
-                                        )
-                                            :
-                                        (
-                                            income['name']
-                                        )
-                                    }
-                                </td>
-                                <td>
-                                    { this.state.editing.transaction_id === income.transaction_id ? 
-                                        (
-                                            <label>
-                                                <select value={income.finances_category} onChange={this.handleEdit('finances_category')}>
-                                                    <option value="other">Other</option>
-                                                    <option value="primary income">Primary Income</option>
-                                                    <option value="tax return">Tax Return</option>
-                                                    <option value="bonus income">Bonus Income</option>
-                                                </select>
-                                            </label>
-                                        )
-                                            :
-                                        (
-                                            income.finances_category
-                                        )
-                                    }
-
-                                </td>
-                                <td onClick={this.handleDelete(income)} style={{ cursor: 'pointer' }}>X</td>
-                                { this.state.editing.transaction_id === income.transaction_id ?
-                                    (<td><button onClick={this.saveEdit}>save</button></td>)
+                <div style={{ marginRight: '10px', marginLeft: '10px', marginBottom: '10px', cursor: 'pointer', display: 'flex', flexDirection: 'row', borderBottom: '2px solid black'}} >
+                    <div style={{ width: '40px',marginBottom: '5px'}}>Date</div>
+                    <div style={{ maxWidth: '195px', marginRight: 'auto', marginLeft: '15px' }}>Name</div>
+                    <div style={{ width: '50px', marginLeft: '15px'}}>Amount</div>
+                </div>
+                {this.props.incomes.map((income, i) => 
+                    
+                    (<>
+                        <div style={{ marginRight: '10px', marginLeft: '10px', cursor:'pointer', display: 'flex', flexDirection: 'row' }} onClick={this.toggleExpand(i)} key={income['transaction_id']}>
+                            <div style={{ width: '40px',marginBottom: '5px'}}>{ this.formatDate(income['date']) }</div>
+                            <div style={{ maxWidth: '245px', marginRight: 'auto', marginLeft: '15px', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}>
+                                { this.state.editing.transaction_id === income.transaction_id ? 
+                                    (
+                                        <input onClick={ this.suppressClick } type='text' value={this.state.editing.name} onChange={ this.handleEdit('name') }></input>
+                                    )
                                         :
-                                    (null)
+                                    ( income['name'] )
                                 }
-                            </tr>)
-                        )}
-                    </tbody>
-                </table>
-                <h1>Expenses</h1>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Date</th>
-                            <th>Name</th>
-                            <th>Amount</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.props.expenses.map((expense, i) => 
+                            </div>
+                            <div style={{ width: '50px', marginLeft: '15px'}}>{income['amount']}</div>
+                        </div>
+                        <div style={{ marginRight: '10px', marginLeft: '10px', marginBottom: '10px', cursor: 'pointer', display: 'flex', flexDirection: 'row', borderBottom: '2px solid lightgrey'}} onClick={this.toggleExpand(i)} className="expander" id={'expense_' + i + '_info'}>
+                            <div style={{ width: '40px', marginBottom: '5px'}}>
+                                { this.state.editing.transaction_id === income.transaction_id ? 
+                                    (
+                                        <button onClick={this.saveEdit}>Save</button>
+                                    )
+                                        :
+                                    (
+                                        <button onClick={this.setEdit(income)}>Edit</button>
+                                    )
+                                }
+                            </div>
+                            <div style={{ maxWidth: '195px', marginRight: 'auto', marginLeft: '15px'}}>
+                                { this.state.editing.transaction_id === income.transaction_id ?
+                                    (
+                                        <select value={this.state.editing.finances_category} onChange={ this.handleEdit('finances_category') } onClick={ this.suppressClick }>
+                                            <option value="other">Other</option>
+                                            <option value="primary income">Primary Income</option>
+                                            <option value="tax return">Tax Return</option>
+                                            <option value="bonus income">Bonus Income</option>
+                                        </select>
+                                    )
+                                        :
+                                    (income['finances_category'])
+                                }
+                            </div>
+                            <div style={{ width: '50px', marginLeft: '15px' }} onClick={this.handleDelete(income)}>X</div>
+                        </div>
+                    </>)
+                )}
+                <h1 id="expenses">Expenses</h1>
+                <div style={{ marginRight: '10px', marginLeft: '10px', marginBottom: '10px', cursor: 'pointer', display: 'flex', flexDirection: 'row', borderBottom: '2px solid black'}} >
+                    <div style={{ width: '40px',marginBottom: '5px'}}>Date</div>
+                    <div style={{ maxWidth: '195px', marginRight: 'auto', marginLeft: '15px' }}>Name</div>
+                    <div style={{ width: '50px', marginLeft: '15px'}}>Amount</div>
+                </div>
+                {this.props.expenses.map((expense, i) => 
 
-                            (<>
-                                <tr style={{ cursor:'pointer' }} onClick={this.toggleExpand(i)} key={expense['transaction_id']}>
-                                    <td align="left" >{ this.formatDate(expense['date']) }</td>
-                                    <td align="left">
-                                        { this.state.editing.transaction_id === expense.transaction_id ? 
-                                            (
-                                                <input type='text' value={this.state.editing.name} onChange={ this.handleEdit('name') }></input>
-                                            )
-                                                :
-                                            ( expense['name'] )
-                                        }
-                                    </td>
-                                    <td>{expense['amount']}</td>
-                                    { /*<td>
-                                        { this.state.editing.transaction_id === expense.transaction_id ? 
-                                            (
-                                                <label>
-                                                    <select value={this.state.editing.finances_category} onChange={ this.handleEdit('finances_category') }>
-                                                        <option value="other">Other</option>
-                                                        <option value="rent/parking">Rent/Parking</option>
-                                                        <option value="utilities">Utilities</option>
-                                                        <option value="car payment">Car Payment</option>
-                                                        <option value="car insurance">Car Insurance</option>
-                                                        <option value="gas/transportation">Gas/Transportation</option>
-                                                        <option value="food out">Food Out</option>
-                                                        <option value="alcohol">Alcohol</option>
-                                                        <option value="groceries">Groceries</option>
-                                                        <option value="subscriptions">Subscriptions</option>
-                                                        <option value="personal care">Personal Care</option>
-                                                        <option value="phone bill">Phone Bill</option>
-                                                        <option value="gym">Gym</option>
-                                                        <option value="entertainment">Entertainment</option>
-                                                        <option value="discretionary">Discretionary</option>
-                                                    </select>
-                                                </label>
-                                            )
-                                                :
-                                            (
-                                                expense.finances_category
-                                            )
-                                        }
-                                        
-                                    </td>
-                                    <td onClick={this.handleDelete(expense)} style={{ cursor: 'pointer' }}>X</td>}
-                                    { this.state.editing.transaction_id === expense.transaction_id ?
-                                        (<td><button onClick={this.saveEdit}>save</button></td>)
-                                            :
-                                        (null)
-                                    } */ }
-                                </tr>
-                                <div className="expander" id={'expense_' + i + '_info'}>
-                                    <p>{ expense.finances_category }</p>
-                                </div>
-                            </>)
-                        )}
-                    </tbody>
-                </table>
+                    (<>
+                        <div style={{ marginRight: '10px', marginLeft: '10px', cursor:'pointer', display: 'flex', flexDirection: 'row' }} onClick={this.toggleExpand(i)} key={expense['transaction_id']}>
+                            <div style={{ width: '40px',marginBottom: '5px'}}>{ this.formatDate(expense['date']) }</div>
+                            <div style={{ maxWidth: '245px', marginRight: 'auto', marginLeft: '15px', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}>
+                                { this.state.editing.transaction_id === expense.transaction_id ? 
+                                    (
+                                        <input onClick={ this.suppressClick } type='text' value={this.state.editing.name} onChange={ this.handleEdit('name') }></input>
+                                    )
+                                        :
+                                    ( expense['name'] )
+                                }
+                            </div>
+                            <div style={{ width: '50px', marginLeft: '15px'}}>{expense['amount']}</div>
+                        </div>
+                        <div style={{ marginRight: '10px', marginLeft: '10px', marginBottom: '10px', cursor: 'pointer', display: 'flex', flexDirection: 'row', borderBottom: '2px solid lightgrey'}} onClick={this.toggleExpand(i)} className="expander" id={'expense_' + i + '_info'}>
+                            <div style={{ width: '40px', marginBottom: '5px'}}>
+                                { this.state.editing.transaction_id === expense.transaction_id ? 
+                                    (
+                                        <button onClick={this.saveEdit}>Save</button>
+                                    )
+                                        :
+                                    (
+                                        <button onClick={this.setEdit(expense)}>Edit</button>
+                                    )
+                                }
+                            </div>
+                            <div style={{ maxWidth: '195px', marginRight: 'auto', marginLeft: '15px'}}>
+                                { this.state.editing.transaction_id === expense.transaction_id ?
+                                    (
+                                        <select value={this.state.editing.finances_category} onChange={ this.handleEdit('finances_category') } onClick={ this.suppressClick }>
+                                            <option value="other">Other</option>
+                                            <option value="rent/parking">Rent/Parking</option>
+                                            <option value="utilities">Utilities</option>
+                                            <option value="car payment">Car Payment</option>
+                                            <option value="car insurance">Car Insurance</option>
+                                            <option value="gas/transportation">Gas/Transportation</option>
+                                            <option value="food out">Food Out</option>
+                                            <option value="alcohol">Alcohol</option>
+                                            <option value="groceries">Groceries</option>
+                                            <option value="subscriptions">Subscriptions</option>
+                                            <option value="personal care">Personal Care</option>
+                                            <option value="phone bill">Phone Bill</option>
+                                            <option value="gym">Gym</option>
+                                            <option value="entertainment">Entertainment</option>
+                                            <option value="discretionary">Discretionary</option>
+                                        </select>
+                                    )
+                                        :
+                                    (expense['finances_category'])
+                                }
+                            </div>
+                            <div style={{ width: '50px', marginLeft: '15px' }} onClick={this.handleDelete(expense)}>X</div>
+                        </div>
+                    </>)
+                )}
             </div>
         )
     }
